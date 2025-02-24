2ed9e910bf97d9f9226b9adb045c227c11669e20
Jibs005 <jayvieomongos8@gmail.com>
2025-02-24 23:40:14 +0800

    java
    added java application

---
+++ omongos_it13/src/omongos_it13/Omongos_it13.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package omongos_it13;
+
+/**
+ *
+ * @author stephanina
+ */
+public class Omongos_it13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }package laboratory_exercise_2;
+
+public class Laboratory_Exercise_2 {
+    public static void main(String[] args) {
+        
+        int num1 = 25;
+        int num2 = 78;
+        int num3 = 87;
+
+        
+        System.out.println("FIRSTNUMBER " + num1);
+        System.out.println("SECONDNUMBER " + num2);
+        System.out.println("THIRDNUMBER " + num3);
+
+       
+        GreatestNumber.findGreatest(num1, num2, num3);
+    }
+}
+
+class GreatestNumber {
+    public static void findGreatest(int num1, int num2, int num3) {
+        int greatest = num1;
+        if (num2 > greatest) {
+            greatest = num2;
+        }
+        if (num3 > greatest) {
+            greatest = num3;
+        }
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
+
+    
+}
